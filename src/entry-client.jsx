101ab7b2dc060d5e2f2d2b79c153202c7128aa60
Abdul\AK <mailAbdul@gmail.com>
2025-03-13 07:04:01 -0400

    working partial

--- src/entry-client.jsx
+++ src/entry-client.jsx
@@ -2,4 +2,4 @@ import { hydrate } from 'solid-js/web';
 import Button from './button.jsx';
 import Home from './home.jsx';
 
-hydrate(() => <Home />, document.getElementById('root'));
+hydrate(() => <Button />, document.getElementById('button-placeholder'));
--- src/home.jsx
+++ src/home.jsx
@@ -1,12 +1,22 @@
+import { NoHydration, Hydration } from 'solid-js/web';
 import Button from './button.jsx';
+import Nav from './nav.tsx';
+
 const Home = () => {
     return (
-        <div>
-            <h1>Home</h1>
-            <div id="button-placeholder">
-                <Button />
+        <NoHydration>
+            <div>
+                <h1>Home</h1>
+                <div>
+                    <Nav />
+                </div>
+                <div id="button-placeholder">
+                    <Hydration>
+                        <Button />
+                    </Hydration>
+                </div>
             </div>
-        </div>
+        </NoHydration>
     )
 }
 
